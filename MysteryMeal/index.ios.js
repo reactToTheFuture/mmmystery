a25535c78741ca362a9ffce2e85bfae86e0c694a
Steve Marreros <lee.marreros@pucp.pe>
2015-08-31 22:55:51 -0700

    [feature] Implements button for next step in walking distance

--- MysteryMeal/index.ios.js
+++ MysteryMeal/index.ios.js
@@ -6,7 +6,8 @@
 
 var React = require('react-native');
 var MapDisplaySection = require('./components/MapSection.io.js');
-var mapbox_api = require('./utils/mapbox-api');
+var DisplayDirections = require('./components/DisplayDirections.io.js');
+
 
 let {
   AppRegistry,
@@ -19,27 +20,22 @@ let styles = StyleSheet.create({
   map: {
     flex: 1,
   },
+  container: {
+    paddingTop: 30,
+    flex: 1,
+    flexDirection: 'column',
+  },
 });
-
-https://api.mapbox.com/v4/directions/mapbox.driving/-122.42,37.78;-77.03,38.91.json?access_token=pk.eyJ1IjoicWFpa2VuIiwiYSI6IjU5MjJjZDlmYjdkYzlmM2UwMDAzMzU1M2ZiOTYxYTQ2In0.SMLCH9SEIW3otXy40SNiKw
-
-mapbox_api.getDirections({
-  'lat': 37.78,
-  'lng': -122.42
-}, {
-  'lat': 38.91,
-  'lng': -77.03
-})
-.then(function(data) {
-  console.log(data);
-})
 
 class MysteryMeal extends React.Component{
   render() {
     return (
-      <View style={styles.map}>
-        <MapDisplaySection />
-      </View>
+        <View style={styles.container}>
+          <DisplayDirections />
+          <View style={styles.map}>
+            <MapDisplaySection />
+          </View>
+        </View>
     );
   }
 }
