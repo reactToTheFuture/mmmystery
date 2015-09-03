3b1a97d4c75f9f5191e0b84c2a9f12ab6f090e1a
Steve Marreros <lee.marreros@pucp.pe>
2015-09-02 21:04:17 -0700

    [Save] Add examples of sync and async tests in Jasmine

--- __tests__/MapSection-test.js
+++ __tests__/MapSection-test.js
@@ -1,7 +1,7 @@
-'use strict';
+jest.dontMock('../app/components/Main');
 
-describe('MapSection', () => {
-  it('does sample test', () => {
-    expect(null).toBe(null);
+describe('sum', function() {
+  it('adds 1 + 2 to equal 3', function() {
+    expect(1 + 2).toBe(3);
   });
 });
