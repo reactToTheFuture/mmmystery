a231f0f33835533b7ca51faa6afb79f2fd63a92f
Phil Keys <phil@phillipkeys.com>
2015-09-09 19:47:30 -0700

    [Feature] Implements color variable as well as new Display font

--- globalVariables.js
+++ globalVariables.js
@@ -3,10 +3,12 @@ module.exports = {
   mediumText: '#5B6675', // Blue gray
   lightText: '#7F90A6', // Lighter blue gray
 
-  primary: '#FFBF00',  // Gold
+  primary: '#FFCE00',  // Gold
   primaryDark: '#FEAF00',  // Dark gold
   primaryLight: '#FFD968', // Lght gold
 
   secondary: '#F3F9FE', // light light blue gray
-  primaryDeep: '#F78914' // orange
+  primaryDeep: '#F78914', // orange
+
+  yellowWhite: '#fdeebd'
 }
